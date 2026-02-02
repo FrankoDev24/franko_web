d5df541fa7fcf2e361c0615a265ef39ff712845a
Owusua22 <nanaowusua1996@gmail.com>
2026-02-02 15:19:50 +0000

    update

--- Franko_web/src/Pages/AdminPages/Orders/Orders.jsx
+++ Franko_web/src/Pages/AdminPages/Orders/Orders.jsx
@@ -21,7 +21,7 @@ const Orders = () => {
   // Agent names for filtering
   const agentNames = [
  "Peggy Andoh", "sarah koffie", 
-    "Florence Gbeve", "Regina Baah", "Gideon Obiri", "Hannah Jethro", "Mavis Attipoe", "Sadam Ansamah","Judith Tsegah"
+    "Florence Gbeve", "Regina Baah", "Dorcas ", "Hannah Jethro", "Mavis Attipoe", "Sadam Ansamah","Judith Tsegah","Dorcas Kumaku","	Roseline Boateng"
   ];
 
   // State management
